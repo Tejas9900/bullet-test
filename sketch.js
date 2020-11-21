d926db9fa73a7bb5f3fd5c70f19c896c9a0afa66
Tejas9900 <73866975+Tejas9900@users.noreply.github.com>
2020-11-21 19:42:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bullet, wall, speed, weight, thickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+ 
+  speed = random(55, 90);
+  weight = random(400, 1500);
+  thickness = random(20, 80);
+
+  bullet = createSprite(200, 200, 50, 10);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "gray";
+
+  wall = createSprite(1500, 200, thickness, height/2);
+  wall.shapeColor = "blue";
+}
+
+function draw() {
+  background(0);
+
+  if(wall.x - bullet.x < bullet.width/2 + wall.width/2){
+    bullet.velocityX = 0;
+    
+    var deformation=0.5* weight* speed* speed/22509;
+
+   if(deformation>180){
+     bullet.shapeColor = "red";
+   }
+   if(deformation>100 && deformation<180){
+     bullet.shapeColor = "yellow";
+   }
+   if(deformation<100){
+     bullet.shapeColor = "green";
+   }
+  }
+
+  drawSprites();
+}
